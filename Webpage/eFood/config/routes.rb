8f29343e2d9860008be59252c62fff3bb4e01cad
Chrono S <shad0w71245@gmail.com>
2017-07-28 17:52:40 +0000

    seeds

--- Webpage/eFood/config/routes.rb
+++ Webpage/eFood/config/routes.rb
@@ -2,14 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#home'
   get 'signup', to: 'welcome#new'
-<<<<<<< HEAD
-  get 'chef_signup', to: 'welcome#newChef'
-  get 'login', to: 'welcome#login'
-  
-  resources :dishes, only: [:index, :create, :show, :update, :destroy]
-end
-=======
   get 'get/get_client', to: 'apis#get_clients_to_json'
   post 'post/add_clients', to: 'apis#create_clients_to_json'
 end
->>>>>>> 6ad289e9feb1633a568b0ce78f157ee1c4aa8875
